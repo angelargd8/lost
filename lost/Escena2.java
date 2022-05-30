ffda318279586a70635e036e52ba9c3ef3c27788
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2022-05-30 17:50:10 -0600

    creacion de escena 1 y 2

---
+++ lost/Escena2.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Escena2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Escena2 extends World
+{
+
+    /**
+     * Constructor for objects of class Escena2.
+     * 
+     */
+    public Escena2()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
