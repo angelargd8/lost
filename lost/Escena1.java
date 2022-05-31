49ad0499b586fefe532da3d3481cbfb8ebef4837
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2022-05-30 22:03:44 -0600

    escena 1

--- lost/Escena1.java
+++ lost/Escena1.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Escena1 extends World
 {
-
+    private int opcion = 0;
     /**
      * Constructor for objects of class Escena1.
      * 
@@ -17,5 +17,23 @@ public class Escena1 extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        texto();
+    }
+    
+    private void texto()//prepara la pantalla con los objetos necesarios
+    {
+        addObject(new texto_1(), 300, 200);
+
+    }
+    public void act(){//permite que el jugador pueda seleccionar las opciones del menu
+        if(Greenfoot.isKeyDown("SPACE")||Greenfoot.isKeyDown("ENTER")){
+            switch(opcion){
+            case 0:
+                Greenfoot.setWorld(new Escena2());
+            //case 1:
+                //Greenfoot.stop();
+                //break;
+            }
+        }
     }
 }
--- lost/texto_1.java
+++ lost/texto_1.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class titulo here.
+ * Write a description of class texto_1 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class titulo extends Actor
+public class texto_1 extends Actor
 {
     /**
-     * Act - do whatever the titulo wants to do. This method is called whenever
+     * Act - do whatever the texto_1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
