2fa48759552624e639bd987f4b333164da80a9ea
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2022-05-31 15:03:31 -0500

    creación escenas 4, 5, 6 y 7

---
+++ lost/Escena6.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Escena6 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Escena6 extends World
+{
+
+    /**
+     * Constructor for objects of class Escena6.
+     * 
+     */
+    public Escena6()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
