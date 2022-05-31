29ef3bcc2fe18f8abbeb4fc113f902291a7d6257
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2022-05-31 15:19:47 -0500

    escena88

---
+++ lost/Escena88.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Escena88 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Escena88 extends World
+{
+
+    /**
+     * Constructor for objects of class Escena88.
+     * 
+     */
+    public Escena88()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
