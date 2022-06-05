c026c098dd0587c9633c26dff06bfa940c5a36be
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2022-06-05 14:54:21 -0500

    final 1 y final 2

---
+++ lost/finalganador.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class finalganador here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class finalganador extends World
+{
+
+    /**
+     * Constructor for objects of class finalganador.
+     * 
+     */
+    public finalganador()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
